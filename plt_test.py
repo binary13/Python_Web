45d6c3cc7fe3a00749305f959009a2e89ecb9295
binary13 <justinmneil@gmail.com>
2016-01-02 11:27:33 -0500

    Modified matplotlib.pyplot test file

--- plt_test.py
+++ plt_test.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4,5], [6,7,8,9,10])
-
+x = [1,2,3,4,5]
+y1 = [4,3,1,7,10]
+y2 = [5,6,9,21,76]
+plt.plot(x, y1, label='Europe')
+plt.plot(x, y2, label = "United States")
+plt.title("Recent Rain of Terror")
+plt.xlabel('Year since 1999')
+plt.ylabel('Alien Abductions')
+plt.legend()
 plt.show()
-
 print('Got here.')
