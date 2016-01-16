0b3017ea67ed24f81decc31653453263600ec3a8
binary13 <justinmneil@gmail.com>
2016-01-16 11:23:06 -0500

    Worked on blackjack

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,41 @@
 from random import randint
 
+class Card(object):
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __str__(self):
+        re_str = ""
+        re_str += self.suit[0]
+        try: re_str += int(self.value)
+        except:
+            if
+
+
+class Deck(object):
+
+
+class Player(object):
+
+    def __init__(self, name='Justin', money=2000):
+        self.money = money
+        self.name = name
+
+    def bet(self):
+        return
+
+    def hit(self):
+        return
+
+    def stand(self):
+        return
+
+
+class Dealer(Player):
+
+    def __init__(self):
+        return
+
+
